6af91cf8bec9a405fa53ccc572f31123e5bbb3ca
fabotse <fabotse@gmail.com>
2022-10-04 11:39:51 -0300

    Admin and Alter Models

--- livro/admin.py
+++ livro/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Livro, Emprestimo
 
 # Register your models here.
+admin.site.register(Livro)
+admin.site.register(Emprestimo)
--- livro/models.py
+++ livro/models.py
@@ -5,13 +5,16 @@ from django.db import models
 class Livro(models.Model):
     nome = models.CharField(max_length=100)
     autor = models.CharField(max_length=100)
-    co_autor = models.CharField(max_length=100)
+    co_autor = models.CharField(max_length=100, blank=True, null=True)
     data_cadastro = models.DateTimeField(auto_now_add=True)
     emprestado = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.nome
+
 
 class Emprestimo(models.Model):
-    nome_emprestado = models.DateTimeField()
-    data_emprestimo = models.DateTimeField()
-    data_devolucao = models.DateTimeField()
-    tempo_duracao = models.DateTimeField()
+    nome_emprestado = models.CharField(max_length=200, blank=True)
+    data_emprestimo = models.DateTimeField(blank=True)
+    data_devolucao = models.DateTimeField(blank=True)
+    tempo_duracao = models.DateTimeField(blank=True)
